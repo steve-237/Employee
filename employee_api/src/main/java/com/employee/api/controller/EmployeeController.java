4d692a447241f4555df632f90c8938e77760466d
steve-237 <steve0peg@gmail.com>
2025-03-04 14:39:23 +0100

    add remaining controllers

--- employee_api/src/main/java/com/employee/api/controller/EmployeeController.java
+++ employee_api/src/main/java/com/employee/api/controller/EmployeeController.java
@@ -3,8 +3,9 @@ package com.employee.api.controller;
 import com.employee.api.model.Employee;
 import com.employee.api.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class EmployeeController {
@@ -15,5 +16,53 @@ public class EmployeeController {
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
+    }
+
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.saveEmployee(employee);
+    }
+
+    @GetMapping("/employee/{id}")
+    public Employee getEmployee(@PathVariable("id") final Long id) {
+        Optional<Employee> employee = employeeService.getEmployee(id);
+        return employee.orElse(null);
+    }
+
+    @PutMapping("/employee/{id}")
+    public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
+        Optional<Employee> employeeOptional = employeeService.getEmployee(id);
+        if (employeeOptional.isPresent()) {
+            Employee employeeToUpdate = employeeOptional.get();
+
+            String firstName = employeeToUpdate.getFirstName();
+            if(firstName != null && !firstName.isEmpty()) {
+                employeeToUpdate.setFirstName(firstName);
+            }
+
+            String lastName = employeeToUpdate.getLastName();
+            if(lastName != null && !lastName.isEmpty()) {
+                employeeToUpdate.setLastName(lastName);
+            }
+
+            String mail = employeeToUpdate.getMail();
+            if(mail != null && !mail.isEmpty()) {
+                employeeToUpdate.setMail(mail);
+            }
+
+            String password = employeeToUpdate.getPassword();
+            if(password != null && !password.isEmpty()) {
+                employeeToUpdate.setPassword(password);
+            }
+            employeeService.saveEmployee(employeeToUpdate);
+            return employeeToUpdate;
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteEmployee(@PathVariable("id") final Long id) {
+        employeeService.deleteEmployee(id);
     }
 }
--- employee_api/src/main/java/com/employee/api/service/EmployeeService.java
+++ employee_api/src/main/java/com/employee/api/service/EmployeeService.java
@@ -15,18 +15,36 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    /**
+     *
+     * @param id
+     * @return
+     */
     public Optional<Employee> getEmployee(final Long id) {
         return employeeRepository.findById(id);
     }
 
+    /**
+     *
+     * @return
+     */
     public Iterable<Employee> getEmployees() {
         return employeeRepository.findAll();
     }
 
+    /**
+     *
+     * @param id
+     */
     public void deleteEmployee(final Long id) {
         employeeRepository.deleteById(id);
     }
 
+    /**
+     *
+     * @param employee
+     * @return
+     */
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
