45fdf8c69e52f791e4db33ebf5dfce85ebbe796c
steve-237 <steve0peg@gmail.com>
2025-03-04 14:55:00 +0100

    add comment in the controller class

--- employee_api/src/main/java/com/employee/api/controller/EmployeeController.java
+++ employee_api/src/main/java/com/employee/api/controller/EmployeeController.java
@@ -13,22 +13,41 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    /**
+     * Get the list of all employees
+     * @return a list of all employees
+     */
     @GetMapping("/employees")
     public Iterable<Employee> getEmployees() {
         return employeeService.getEmployees();
     }
 
+    /**
+     * Create a new employee
+     * @param employee - employee object to create
+     * @return the new employee
+     */
     @PostMapping("/employee")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeService.saveEmployee(employee);
     }
 
+    /**
+     * Get an employee based on the id
+     * @param id - id of the employee to retrieve
+     * @return An object employee
+     */
     @GetMapping("/employee/{id}")
     public Employee getEmployee(@PathVariable("id") final Long id) {
         Optional<Employee> employee = employeeService.getEmployee(id);
         return employee.orElse(null);
     }
 
+    /**
+     * Update an employee based on the id
+     * @param id - id of the employee to be updated
+     * @return An object employee
+     */
     @PutMapping("/employee/{id}")
     public Employee updateEmployee(@PathVariable("id") final Long id, @RequestBody Employee employee) {
         Optional<Employee> employeeOptional = employeeService.getEmployee(id);
@@ -61,6 +80,10 @@ public class EmployeeController {
         }
     }
 
+    /**
+     * Delete an employee
+     * @param id of the employee to be deleted
+     */
     @DeleteMapping("/delete/{id}")
     public void deleteEmployee(@PathVariable("id") final Long id) {
         employeeService.deleteEmployee(id);
